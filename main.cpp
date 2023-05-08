7a9b99b437610064fd2d4735ade2a6e0dcec4c5f
BartTheBartender <bartosz.furmanek@student.uj.edu.pl>
2023-05-08 21:04:51 +0200

    trzeci wielowątkowy

--- main.cpp
+++ main.cpp
@@ -8,45 +8,17 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-//~ int main() {
-	
-	//~ Long base = 4;
-	
-	//~ while(true){
-
-		//~ ostringstream name_dir;
-		//~ name_dir << "Wyniki/txt/Z-" << base;
-		//~ filesystem::create_directory(name_dir.str());
-		
-		//~ for(Long size = 1; size < (Long)10000; ++size){
-
-		    //~ auto start_time = std::chrono::high_resolution_clock::now();
-			
-			//~ ostringstream name;
-			//~ name << "Wyniki/txt/Z-" << base << "/Z-" << base << "-attempt-" << size << ".txt";
-
-			//~ ofstream file;
-			//~ file.open(name.str());
-
-			//~ file << Relation::generate(base,size);
-			//~ file.close();			
-			
-			//~ auto end_time = std::chrono::high_resolution_clock::now();
-			//~ auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-			
-			//~ Relation::reset();
-			//~ if(duration.count() > 3600000) break;
-		//~ }
-		
-		//~ ++base;
-	//~ }
-
-//~ }
 
 int main() {
 	
-	Long base = 3;
-	Long size = 2;
+	Long base;
+	Long size;
+	
+	cout << "Generuj relacje w modułach nad Z/";
+	cin >> base;
+	cout << "do wymiaru ";
+	cin >> size;
+	cout << endl;
 
 	Relation::generate(base, size);
 	
@@ -54,13 +26,18 @@ int main() {
 		ostringstream name_dir_Filip;
 		name_dir << "Wyniki/txt/Z-" << base;
 		name_dir_Filip << "Wyniki/txt-Filip/Z-" << base;		
-			
+
+
+		if (!fs::exists("Wyniki")) {
+			fs::create_directory("Wyniki");
+		}	
 		if (!fs::exists("Wyniki/txt")) {
 			fs::create_directory("Wyniki/txt");
 		}
 		if (!fs::exists("Wyniki/txt-Filip")) {
 			fs::create_directory("Wyniki/txt-Filip");
 		}		
+		
 		if (!fs::exists(name_dir.str())) {
 			fs::create_directory(name_dir.str());
 		}
